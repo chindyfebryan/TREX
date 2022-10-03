0cf6c574430bb2e0f81b03d111f619a9efa12ca8
Chindy febryan <45525155+chindyfebryan@users.noreply.github.com>
2022-10-03 11:31:56 +0800

    update

--- js/app.js
+++ js/app.js
@@ -1,25 +1,44 @@
-function navFunction() {
-    var x = document.getElementById("nav");
-    if (x.className === "header-right") {
-      x.className += " responsive";
-    } else {
-      x.className = "header-right";
-    }
-}
+var arrDates=new Array();
+var arrCategories=new Array();
+var arrAmounts=new Array();
+var arrId=new Array();
 
 function addRow() {    
-    var date = document.getElementById("date");
-    var category = document.getElementById("category");
-    var amount = document.getElementById("amount");
+ 
+    var date = document.getElementById("date").value;
+    var category = document.getElementById("category").value;
+    var amount = document.getElementById("amount").value;
     var table = document.getElementById("tableData");
 
     var rowCount = table.rows.length;
     var row = table.insertRow(rowCount);
 
-    row.insertCell(0).innerHTML= date.value;
-    row.insertCell(1).innerHTML= category.value;
-    row.insertCell(2).innerHTML= amount.value;
-    row.insertCell(3).innerHTML= '<input type="button" value = "Delete" onClick="Javacsript:deleteRow(this)"> <input type="button" value = "Sum" onClick="Javacsript:sum(this)">';
+    row.insertCell(0).innerHTML= date;
+    row.insertCell(1).innerHTML= category;
+    row.insertCell(2).innerHTML= amount;
+    row.insertCell(3).innerHTML= '<input type="button" value = "Delete" onClick="Javacsript:deleteRow(this)">';
+    row.insertCell(4).innerHTML= '<input type="button" value = "Sum" onClick="Javacsript:sum(this)">';
+    
+    var content="<b>Data Entered by User :</b><br>";
+    var input = document.getElementsByName('txtval[]');
+               
+    for (var x = 0; x < input.length; x++) 
+    {
+      var a = input[x];
+      content = content + "myarray[" + x + "].value= " + a.value + "<br> "; 
+      document.getElementById('display').innerHTML = content+"</b>";  
+    }
+
+    arrDates[arrDates.length]=date;    
+    arrCategories=[arrCategories.length]=category;
+    arrAmounts[arrAmounts.length]=amount;
+
+    var content="<b>Data Entered by User :</b><br>";
+  content+= [...arrDates]+"</br>";
+  content+=[...arrCategories]+"</br>";
+  content+=[...arrAmounts]+"</br>";
+ 
+  document.getElementById('display').innerHTML = content;
 }
 
 function sum() {
